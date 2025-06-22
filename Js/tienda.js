5ebf23d6a9a1278bed9a61e08a992162426ff9fe
karen-codes <kcabascangoa@est.ups.edu.ec>
2025-06-21 21:12:24 -0500

    Inicio del codigi JS tienda

---
+++ Js/tienda.js
@@ -0,0 +1,8 @@
+const productos = [
+  { id: 1, nombre: "Pastel de Chocolate", precio: 15.0 },
+  { id: 2, nombre: "Pastel de Vainilla", precio: 13.5 },
+  { id: 3, nombre: "Pastel de Frutas", precio: 16.0 },
+  { id: 4, nombre: "Red Velvet", precio: 17.0 },
+  { id: 5, nombre: "Pastel de Zanahoria", precio: 14.0 },
+  { id: 6, nombre: "Cheesecake", precio: 18.5 },
+];
